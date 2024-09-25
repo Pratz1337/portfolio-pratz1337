5f7307a2581f381f2cd0f9da3a40f3309b72b79b
Pratz1337 <sayalprathmesh8@gmail.com>
2024-09-25 14:12:01 +0530

    portfolio

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+      domains: ['https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjQFtEyXJg4vHdkl_XpdTlwMRzJOg_9g5uhQ&s'], // Add this if you're using external image URLs
+    },
+  }
+  
+  export default nextConfig;
+  
